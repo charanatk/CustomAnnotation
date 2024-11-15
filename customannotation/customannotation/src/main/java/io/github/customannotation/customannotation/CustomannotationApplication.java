49d1dbb116352171e295fd5970074a69008e499e
charan <charanatk@gmail.com>
2024-11-15 13:58:18 +0530

    initial commit

---
+++ customannotation/customannotation/src/main/java/io/github/customannotation/customannotation/CustomannotationApplication.java
@@ -0,0 +1,13 @@
+package io.github.customannotation.customannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomannotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomannotationApplication.class, args);
+	}
+
+}
